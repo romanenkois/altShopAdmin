6171ee45d2c963b1af7648b1e6a1174283ac98bb
Illya <romanenkoisnot@gmail.com>
2024-01-10 19:18:29 +0200

    migration to basic css and bootstrap for grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-// @type {import('tailwindcss').Config} 
-module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-    colors: {
-      primary: '#ffdd00',
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+// // @type {import('tailwindcss').Config} 
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//     colors: {
+//       primary: '#ffdd00',
+//     }
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
